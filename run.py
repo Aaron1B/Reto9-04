14da4746f7a07602a38e0d3609a956da5319f495
Aarón Sacristán <aaronsacristan6a@gmail.com>
2025-04-09 12:52:51 +0200

    Cambios9

--- run.py
+++ run.py
@@ -1,15 +1,14 @@
 import gradio as gr
-from hanoi.ui import ui as hanoi_ui 
-from knight.ui import ui as knight_ui  
-from nqueens.ui import ui as nqueens_ui 
+from hanoi.ui import ui as hanoi_ui
+from knight.ui import ui as knight_ui
+
 with gr.Blocks() as app:
-    gr.Markdown("## Juegos Automatizados")
+    gr.Markdown("## Juegos Automatizados 🧠🎮")
 
     with gr.Tab("Torres de Hanoi"):
-        hanoi_ui()  
+        hanoi_ui()
 
     with gr.Tab("Recorrido del Caballo"):
-        knight_ui()  
-    with gr.Tab("Problema de las Reinas"):
-        nqueens_ui()  
-app.launch()
+        knight_ui()
+
+app.launch(server_port=7862)
